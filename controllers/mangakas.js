0941d786c4270f9be5a93a9b7aae455061f64148
Jorge Olivero <joliveroizquierdo@gmail.com>
2017-05-09 23:05:40 -0500

    Mostrndo el ultimo registro que se ingreso

--- controllers/mangakas.js
+++ controllers/mangakas.js
@@ -1,25 +1,15 @@
 const firebase = require('firebase')
 
-firebase.initializeApp({
-	serviceAccount: "./mangas-0e34a00781e8.json",
-	databaseURL: "https://mangas-9325b.firebaseio.com"
-})
+	  firebase.initializeApp({
+			serviceAccount: "./mangas-0e34a00781e8.json",
+			databaseURL: "https://mangas-9325b.firebaseio.com"
+	  })
 
-const db = firebase.database()
+const refMangakas = firebase.database().ref().child('mangakas')
 
 exports.insertarMangakas = (req, res) => {
 
 		let nombreMangaka = req.body.nombre
-
-		/*let refMangakas = db.ref('mangakas').push()
-
-			refMangakas.set({
-
-				nombre : nombreMangaka
-
-			})*/
-
-		let refMangakas = db.ref().child('mangakas')
 
 			refMangakas.push({
 
@@ -27,6 +17,22 @@ exports.insertarMangakas = (req, res) => {
 
 			})
 
-		res.json({mensaje: 'insertarMangaka'})
+			refMangakas.on('child_added', (snap) => {
+			
+				console.log(snap.val())
+
+				let datos = snap.val()
+
+				console.log(datos)
+
+				res.json({datos})
+
+			})
 		
+}
+
+exports.listarMangakas = (req, res) => {
+
+		console.log('listarMangakas')
+
 }
--- routes/rutas.js
+++ routes/rutas.js
@@ -4,5 +4,6 @@ const controllers     = require('../controllers/mangakas')
 
 router.route('/mangakas')
 	  .post(controllers.insertarMangakas)
+	  .get(controllers.listarMangakas)
 
 module.exports = router
